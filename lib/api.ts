a90f0d784669506d795df8c343bedbfbf8094104
hieudevhouse <nguyenminhhieu2003hd@gmail.com>
2026-01-12 19:30:51 +0700

    fix hosting be

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { Task } from './types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9999/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://hosting-ph9i.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
